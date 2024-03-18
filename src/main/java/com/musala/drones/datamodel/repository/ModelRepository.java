40b964767e3c7a3544eda203b9365cdefa8f79f8
Rajinda Dissanayake <rajidissa@hotmail.com>
2024-03-18 11:58:29 +0000

    The code for the drone registration functionality has been updated

--- src/main/java/com/musala/drones/datamodel/repository/ModelRepository.java
+++ src/main/java/com/musala/drones/datamodel/repository/ModelRepository.java
@@ -2,6 +2,8 @@ package com.musala.drones.datamodel.repository;
 
 import com.musala.drones.datamodel.data.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ModelRepository extends JpaRepository<Model, Long> {
 }
