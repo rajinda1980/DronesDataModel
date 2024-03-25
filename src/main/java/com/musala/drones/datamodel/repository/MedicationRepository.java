57222cfca8474bb538a8e327b918f303c9de23cb
Rajinda Dissanayake <rajidissa@hotmail.com>
2024-03-25 16:02:02 +0000

    Added indexes

--- src/main/java/com/musala/drones/datamodel/repository/MedicationRepository.java
+++ src/main/java/com/musala/drones/datamodel/repository/MedicationRepository.java
@@ -2,8 +2,15 @@ package com.musala.drones.datamodel.repository;
 
 import com.musala.drones.datamodel.data.Medication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MedicationRepository extends JpaRepository<Medication, Long> {
+
+    @Query("FROM Medication med WHERE med.drone.serialNumber = :serialNumber")
+    Optional<List<Medication>> findAllByDrone(String serialNumber);
 }
